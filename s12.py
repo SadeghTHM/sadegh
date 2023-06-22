706f84644d80d83fc09160271cb601cf1b719729
SadeghTHM <137379944+SadeghTHM@users.noreply.github.com>
2023-06-22 10:03:44 +0330

    Create s12.py

---
+++ s12.py
@@ -0,0 +1,17 @@
+name = (input('enter the your name:'))
+Lesson1 = int(input('enter ther Lesson:'))
+Lesson2 = int(input('enter ther Lesson:'))
+Lesson3 = int(input('enter ther Lesson:'))
+print (Lesson1 + Lesson2 + Lesson3)
+if Lesson1 + Lesson2 + Lesson3 > 19:
+    print ("A")
+if Lesson1 + Lesson2 + Lesson3 > 18:
+    print ("B")
+if Lesson1 + Lesson2 + Lesson3 > 17:
+    print ("C")
+if Lesson1 + Lesson2 + Lesson3 > 16:
+    print ("D")
+if Lesson1 + Lesson2 + Lesson3 > 15:
+    print ("E")
+else :
+    print ("F")
